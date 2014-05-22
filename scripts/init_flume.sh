79786e69f71f3f2bf49b4f7ad509ead413c8a609
Jia Cao <jerrycircle@gmail.com>
2014-05-22 17:55:18 +0800

    use file channel for flume, and fix init_flume script

--- scripts/init_flume.sh
+++ scripts/init_flume.sh
@@ -12,6 +12,10 @@ mkdir -p /usr/local/flume/plugins/lib
 cp -r /vagrant/flume/*.jar /usr/local/flume/plugins/lib
 chown -R flume:flume /usr/local/flume
 
+mkdir -p /var/lib/flume-ng/file-channel/checkpoint
+mkdir -p /var/lib/flume-ng/file-channel/backup-checkpoint
+mkdir -p /var/lib/flume-ng/file-channel/data
+
 sed -e "s/\${KAFKA_BROKERS}/$1/g" /vagrant/flume/nginx-flume.conf > /etc/flume-ng/conf/flume.conf
 
 cp /vagrant/flume/initd_flume-agent /etc/init.d/flume-ng-agent
